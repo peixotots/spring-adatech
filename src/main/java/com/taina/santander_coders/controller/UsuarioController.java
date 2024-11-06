4674ae2b7680ec1bb42b46fffe30e1b530352e07
Tainá Peixoto <tsouzapeixoto@gmail.com>
2024-11-05 23:43:33 -0300

    feat: Adição de método POST para criação de usuário e interação com banco de dados

--- src/main/java/com/taina/santander_coders/controller/UsuarioController.java
+++ src/main/java/com/taina/santander_coders/controller/UsuarioController.java
@@ -1,20 +1,25 @@
 package com.taina.santander_coders.controller;
 
 import com.taina.santander_coders.model.Usuario;
+import com.taina.santander_coders.service.CriarUsuarioService;
 import com.taina.santander_coders.service.ObterUsuarioService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
 
     private final ObterUsuarioService service;
+    private final CriarUsuarioService criarUsuarioService;
 
-    public UsuarioController(ObterUsuarioService service){
+    public UsuarioController(ObterUsuarioService service, CriarUsuarioService criarUsuarioService) {
         this.service = service;
+        this.criarUsuarioService = criarUsuarioService;
+    }
+
+    @PostMapping
+    public Usuario criarUsuario(@RequestBody Usuario usuario){
+        return criarUsuarioService.executar(usuario);
     }
 
     @GetMapping("/{id}")
