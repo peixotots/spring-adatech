08c4f154fffc7b79b24e2a550a9b20c52c57a912
Tainá Peixoto <tsouzapeixoto@gmail.com>
2024-11-05 23:56:51 -0300

    feat: Ajuste da resposta do HttpStatus para HttpStatus.CREATED no método POST de criação de usuário no UsuarioController

--- src/main/java/com/taina/santander_coders/controller/UsuarioController.java
+++ src/main/java/com/taina/santander_coders/controller/UsuarioController.java
@@ -3,6 +3,8 @@ package com.taina.santander_coders.controller;
 import com.taina.santander_coders.model.Usuario;
 import com.taina.santander_coders.service.CriarUsuarioService;
 import com.taina.santander_coders.service.ObterUsuarioService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,8 +20,8 @@ public class UsuarioController {
     }
 
     @PostMapping
-    public Usuario criarUsuario(@RequestBody Usuario usuario){
-        return criarUsuarioService.executar(usuario);
+    public ResponseEntity<Usuario> criarUsuario(@RequestBody Usuario usuario){
+        return ResponseEntity.status(HttpStatus.CREATED).body(criarUsuarioService.executar(usuario));
     }
 
     @GetMapping("/{id}")
