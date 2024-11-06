b54e08493b628a689a492fa5872989435ab120fe
Tainá Peixoto <tsouzapeixoto@gmail.com>
2024-11-06 00:22:27 -0300

    Refactor: Ajuste da estratégia de geração do ID para a entidade Usuario

--- src/main/java/com/taina/santander_coders/model/Usuario.java
+++ src/main/java/com/taina/santander_coders/model/Usuario.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 public class Usuario {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     @Column(name = "NOME_USUARIO", nullable = false)
     private String nome;
