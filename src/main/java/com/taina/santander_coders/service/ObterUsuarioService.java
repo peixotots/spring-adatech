8fd363a6fd76f22d4d24f07ed90747b9395925a9
Tainá Peixoto <tsouzapeixoto@gmail.com>
2024-11-06 09:59:30 -0300

    feat: Refatoração do método execute para buscar usuário real no banco de dados usando UsuarioRepository

--- src/main/java/com/taina/santander_coders/service/ObterUsuarioService.java
+++ src/main/java/com/taina/santander_coders/service/ObterUsuarioService.java
@@ -1,13 +1,24 @@
 package com.taina.santander_coders.service;
 
 import com.taina.santander_coders.model.Usuario;
+import com.taina.santander_coders.repository.UsuarioRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ObterUsuarioService {
 
+    private final UsuarioRepository repository;
+
+    public ObterUsuarioService(UsuarioRepository repository) {
+        this.repository = repository;
+    }
+
     // Metodo responsável por buscar um usuário no banco de dados pelo id
     public Usuario execute(Long id){
-        return null;
+        Optional<Usuario> usuario = repository.findById(id);
+
+        return usuario.orElseThrow(() -> new RuntimeException(String.format("Usuário com id %d não encontrado", id)));
     }
 }
